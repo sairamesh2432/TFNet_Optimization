931ef22e419b2ef326e3ad2f06a6ef02d8cd8f95
sairamesh2432 <59543272+sairamesh2432@users.noreply.github.com>
2021-03-25 23:43:24 -0400

    Create run_unoptimized_script.sh

---
+++ software_exploration/run_unoptimized_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mem=5gb 
+#SBATCH --time=15:00:00
+
+
+module purge
+module load GCC/6.4.0-2.28 OpenMPI/2.1.2
+module load CUDA/9.0.176 cuDNN/7.0.2-CUDA-9.0.176
+module load Python/3.6.4
+module load Anaconda/3
+
+conda activate base
+python TF_train.py -region ./region.txt -LR 0.001 -output ./fourth_test  # change this output to a new directory when running again
+
+scontrol show job $SLURM_JOB_ID
