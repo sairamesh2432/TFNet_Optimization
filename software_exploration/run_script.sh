ed3801e9c8bbf0a5979b04be37f24017dd284971
sairamesh2432 <59543272+sairamesh2432@users.noreply.github.com>
2021-03-25 23:35:07 -0400

    Update run_script.sh

--- software_exploration/run_script.sh
+++ software_exploration/run_script.sh
@@ -3,13 +3,14 @@
 #SBATCH --mem=5gb 
 #SBATCH --time=15:00:00
 
-TRAIN_SCRIPT=$1
-OUTPUT=$2
 
 module purge
 module load GCC/6.4.0-2.28 OpenMPI/2.1.2
 module load CUDA/9.0.176 cuDNN/7.0.2-CUDA-9.0.176
 module load Python/3.6.4
-python $(TRAIN_SCRIPT) -region ./region.txt -LR 0.001 -output $(OUTPUT)
+module load Anaconda/3
+
+conda activate base
+python lightning_TF_train.py -region ./region.txt -LR 0.001 -output ./third_test  # change this output to a new directory when running again
 
 scontrol show job $SLURM_JOB_ID
